68a4130d5150451cd5d9a6f9da56d03ee5cfdb5c
aryanmanik013 <aryanmanik440@gmail.com>
2023-12-19 17:37:27 +0530

    First commit

---
+++ app/Views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRM</title>
+
+  <?= $this->include("includes/Links") ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <?= $this->include('Dashboard/index') ?>
+
+</body>
+
+</html>
